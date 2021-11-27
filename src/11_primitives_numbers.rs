604f2310328752e38a8b5dd9d64315a09939991e
Mateus <mduraes1994@gmail.com>
2021-11-26 22:50:49 -0300

    🎸 Learning numbers

---
+++ src/11_primitives_numbers.rs
@@ -0,0 +1,16 @@
+
+// Integers
+  // i8, i16, i32, i64, i128
+let x = 10; // ⭐️ Default integer type in Rust is i32
+let y: i8 = -128;
+
+
+// Integers - positive only
+  // u8, u16, u32, u64, u128
+
+let x: i8 = 255;
+
+// Float
+  // f32, f64
+let x = 1.5; // ⭐️ The default float type in Rust is f64
+let y: f64 = 2.0;
